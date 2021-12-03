c0dcc31c0c5297140bee29a1e88dadbb05dbd825
Russell Doughty <russell.doughty@ou.edu>
2021-12-03 13:03:24 -0600

    Update for annual VPM

--- ChloFluo/ChloFluo_vs_VPM_timeseries.jl
+++ ChloFluo/ChloFluo_vs_VPM_timeseries.jl
@@ -21,7 +21,11 @@ vpm  = vpm.data;
 
 gpp_diff = cf .- vpm;
 gpp_diff = permutedims(gpp_diff, [2,1,3])
+vpm      = permutedims(vpm, [2,1,3]) 
 
 heatmap(gpp_diff[:,:,1], title = "VPM", bg = :white, color = :RdBu, clim = (-7.5,7.5))
 
 save_nc(gpp_diff, "/mnt/g/ChloFluo/comps/vpm/ChloFluo-VPM.v01.1deg.CF80.2019.nc", 2019, "gpp_diff", "ChloFluo GPP - VPM GPP", "g C/m-2/day-1");
+
+# Save VPM to calculate annual mean map in R
+save_nc(vpm, "/mnt/g/ChloFluo/comps/vpm/VPM.1deg.2019.nc", 2019, "gpp", "VPM GPP", "g C/m-2/day-1");
