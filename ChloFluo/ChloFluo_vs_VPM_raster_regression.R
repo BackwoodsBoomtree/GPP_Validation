5719440a8a6000853e831448a7e92bd09500894b
Doughty <russell.doughty@ou.edu>
2021-12-03 15:28:56 -0600

    Initial commit

---
+++ ChloFluo/ChloFluo_vs_VPM_raster_regression.R
@@ -0,0 +1,74 @@
+library(raster)
+
+cf_file  <- "G:/ChloFluo/product/v01/1deg/clipfill/ChloFluo.GPP.v01.1deg.CF80.2019.clipfill.nc"
+vpm_file <- "G:/ChloFluo/comps/vpm/VPM.1deg.2019.nc"
+x_name   <- "gpp"
+y_name   <- "gpp"
+out_dir  <- "G:/ChloFluo/comps/vpm/raster_regressions"
+out_name <- "ChlFluo_vs_VPM.v01.1deg.CF80.2019.clipfill.nc"
+f_name   <- NA # Filter by value. Example, error, std, or n. If none use NA.
+f_thresh <- 30  # Values => will be kept
+
+rastlm <- function(x) {
+  full <- length(x)
+  half <- full / 2
+  
+  if (all(is.na(x[1:half])) || all(is.na(x[(half + 1):full]))){ 
+    
+    return(c(NA,NA,NA,NA,NA,NA))
+    
+  } else { 
+    reg       <- lm(x[1:half] ~ x[(half +1):full])
+    s         <- summary(reg)
+    r2        <- s$r.squared
+    pval      <- s$coefficients[8]
+    slope     <- s$coefficients[2]
+    intercept <- s$coefficients[1]
+    rmse      <- sqrt(mean(s$residuals^2))
+    n         <- nobs(reg)
+    
+    return(c(r2, pval, slope, intercept, rmse, n)) 
+  }
+}
+
+
+rast_reg <- function(y_file, x_file, x_name, y_name, out_dir, out_name) {
+  
+  y <- brick(cf_file, varname = y_name)
+  x <- brick(vpm_file, varname = x_name)
+  
+  y <- mask(y, x)
+  x <- mask(x, y)
+  
+  # Filter as needed
+  if (!is.na(f_name)) {
+    print(paste0("Filtering data using: ", f_name))
+    print(paste0("Filter threshold is: ", f_thresh))
+    
+    f <- brick(in_file, varname = f_name)
+    f[f < f_thresh] <- NA
+    
+    y <- mask(y, f)
+    x <- mask(x, f)
+    
+  }
+  
+  # Combine bricks into single brick. lm convention is y~x
+  yx <- stack(y, x)
+  
+  beginCluster(12)
+  lm.result <- clusterR(yx, calc, args = list(fun = rastlm))
+  endCluster()
+  
+  writeRaster(lm.result[[1]], paste0(out_dir, "/", out_name, "_Rsquare.tif"), overwrite = TRUE)
+  writeRaster(lm.result[[2]], paste0(out_dir, "/", out_name, "_Pval.tif"), overwrite = TRUE)
+  writeRaster(lm.result[[3]], paste0(out_dir, "/", out_name, "_Slope.tif"), overwrite = TRUE)
+  writeRaster(lm.result[[4]], paste0(out_dir, "/", out_name, "_Intercept.tif"), overwrite = TRUE)
+  writeRaster(lm.result[[5]], paste0(out_dir, "/", out_name, "_RMSE.tif"), overwrite = TRUE)
+  writeRaster(lm.result[[6]], paste0(out_dir, "/", out_name, "_Nobs.tif"), overwrite = TRUE)
+  
+  remove(x, y, yx) # get it out of memory
+  
+}
+
+rast_reg(cf_file, vpm_file, x_name, y_name, out_dir, out_name)
