664e0711c072947fd659f32bdc0057cfa80ed22a
Doughty <russell.doughty@ou.edu>
2022-11-11 21:21:52 -0600

    Testing for towers in Africa

--- tower_comparisons.R
+++ tower_comparisons.R
@@ -12,7 +12,20 @@ k34_gep    <- read.csv("G:/SIF_comps/figs/Wu_2016/K34_GEP.csv", header = FALSE)[
 k67_gep    <- read.csv("G:/SIF_comps/figs/Wu_2016/K67_GEP.csv", header = FALSE)[,2]
 rja_gep    <- read.csv("G:/SIF_comps/figs/Wu_2016/RJA_GEP.csv", header = FALSE)[,2]
 cax_gep    <- read.csv("G:/SIF_comps/figs/Wu_2016/CAX_GEP.csv", header = FALSE)[,2]
+tch_gpp    <- read.csv("G:/ChloFluo/comps/tower-data/CG-Tch/FLX_CG-Tch_FLUXNET2015_FULLSET_MM_2006-2009_1-4.csv", header = TRUE)
+ank_gpp    <- read.csv("G:/ChloFluo/comps/tower-data/GH-Ank/FLX_GH-Ank_FLUXNET2015_FULLSET_MM_2011-2014_1-4.csv", header = TRUE)
 
+# Get Africa GPP tower data
+tch_gpp_nt <- tch_gpp$GPP_NT_VUT_MEAN
+ank_gpp_nt <- ank_gpp$GPP_NT_VUT_MEAN
+
+tch_gpp_dt <- tch_gpp$GPP_DT_VUT_MEAN
+ank_gpp_dt <- ank_gpp$GPP_DT_VUT_MEAN
+
+tch_gpp_mean <- (tch_gpp_nt + tch_gpp_dt) / 2
+ank_gpp_mean <- (ank_gpp_nt + ank_gpp_dt) / 2
+
+# Get model GPP data
 cf   <- rast(cf_file, subds = "gpp")
 fcom <- rast(fcom_file, subds = "GPP")
 fsat <- rast(fsat_files, subds = "GPP")
@@ -38,14 +51,20 @@ for (i in seq(1, 365, by = 8)) {
 fsat <- aggregate(fsat_8day, 20, fun = mean, na.rm = TRUE)
 
 ## Tower
-k34 <- cbind(-60.2093,-2.6091)
+k34 <- cbind(-60.2093, -2.6091)
 k34 <- vect(k34, crs = "+proj=longlat +ellps=WGS84")
 
-cax <- cbind(-51.4536,-1.7483)
+cax <- cbind(-51.4536, -1.7483)
 cax <- vect(cax, crs = "+proj=longlat +ellps=WGS84")
 
-k67 <- cbind(-54.959,-2.857)
+k67 <- cbind(-54.959, -2.857)
 k67 <- vect(k67, crs = "+proj=longlat +ellps=WGS84")
+
+tch <- cbind(11.6564, -4.2892)
+tch <- vect(tch, crs = "+proj=longlat +ellps=WGS84")
+
+ank <- cbind(-2.6942, 5.2685)
+ank <- vect(ank, crs = "+proj=longlat +ellps=WGS84")
 
 # Timeseries
 cf_k34   <- extract(cf, k34)
@@ -60,6 +79,14 @@ cf_k67   <- extract(cf, k67)
 fcom_k67 <- extract(fcom, k67)
 fsat_k67 <- extract(fsat, k67)
 
+cf_tch   <- extract(cf, tch)
+fcom_tch <- extract(fcom, tch)
+fsat_tch <- extract(fsat, tch)
+
+cf_ank   <- extract(cf, ank)
+fcom_ank <- extract(fcom, ank)
+fsat_ank <- extract(fsat, ank)
+
 cf_k34   <- unlist(cf_k34, use.names = FALSE)[-c(1)]
 fcom_k34 <- unlist(fcom_k34, use.names = FALSE)[-c(1)]
 fsat_k34 <- unlist(fsat_k34, use.names = FALSE)[-c(1)]
@@ -71,6 +98,14 @@ fsat_cax <- unlist(fsat_cax, use.names = FALSE)[-c(1)]
 cf_k67   <- unlist(cf_k67, use.names = FALSE)[-c(1)]
 fcom_k67 <- unlist(fcom_k67, use.names = FALSE)[-c(1)]
 fsat_k67 <- unlist(fsat_k67, use.names = FALSE)[-c(1)]
+
+cf_tch   <- unlist(cf_tch, use.names = FALSE)[-c(1)]
+fcom_tch <- unlist(fcom_tch, use.names = FALSE)[-c(1)]
+fsat_tch <- unlist(fsat_tch, use.names = FALSE)[-c(1)]
+
+cf_ank   <- unlist(cf_ank, use.names = FALSE)[-c(1)]
+fcom_ank <- unlist(fcom_ank, use.names = FALSE)[-c(1)]
+fsat_ank <- unlist(fsat_ank, use.names = FALSE)[-c(1)]
 
 # Monthly aggregation
 to_month <- function(vals, year) {
@@ -115,11 +150,22 @@ cf_k67_month   <- to_month(cf_k67, 2019)
 fcom_k67_month <- to_month(fcom_k67, 2019)
 fsat_k67_month <- to_month(fsat_k67, 2019)
 
+cf_tch_month   <- to_month(cf_tch, 2019)
+fcom_tch_month <- to_month(fcom_tch, 2019)
+fsat_tch_month <- to_month(fsat_tch, 2019)
+
+cf_ank_month   <- to_month(cf_ank, 2019)
+fcom_ank_month <- to_month(fcom_ank, 2019)
+fsat_ank_month <- to_month(fsat_ank, 2019)
+
+tch_gpp_mean_month <- (tch_gpp_mean[1:12] + tch_gpp_mean[13:24] + tch_gpp_mean[25:36] + tch_gpp_mean[37:48]) / 4
+ank_gpp_mean_month <- (ank_gpp_mean[1:12] + ank_gpp_mean[13:24] + ank_gpp_mean[37:48]) / 3
+
 
 ## Plot variables
 x            <- 1:12
 x_lab        <- c("J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D")
-y_lab_gpp    <- list(bquote("GPP"), bquote("(g C m"^"-2"*" d"^"-1"*")"))
+y_lab_gpp    <- list(bquote("GPP (g C m"^"-2"*" d"^"-1"*")"))
 y_limit_gpp  <- c(4,12)
 
 mag.cols <- magma(7)
@@ -129,30 +175,30 @@ vir.cols <- viridis(7)
 ### PLOT
 cairo_pdf(out_name, width = 7.5, height = 3.25)
 
-par(mfrow = c(1, 1), oma=c(2.0,6,2.5,0.1))
+par(mfrow = c(1, 1), oma=c(2,3,2.5,0))
 
 # Amazon GPP
 op <- par(mar = c(0.5,0,0,0.5))
 
-plot(x, fsat_k34_month, col = vir.cols[6], type = "l", axes = FALSE, lwd = 1.5, xaxs="i", ylim = y_limit_gpp, ylab = "")
+plot(x, fsat_k34_month, col = vir.cols[6], type = "l", axes = FALSE, lwd = 2, xaxs="i", ylim = y_limit_gpp, ylab = "")
 
-lines(x, fcom_k34_month, col = vir.cols[4], lwd = 1.5)
-lines(x, k34_gep, col = "black", lwd = 1.5)
-lines(x, cf_k34_month, col = vir.cols[2], lwd = 1.5)
+lines(x, fcom_k34_month, col = vir.cols[4], lwd = 2)
+lines(x, k34_gep, col = "black", lwd = 2)
+lines(x, cf_k34_month, col = vir.cols[2], lwd = 2)
 
-axis(1, tck = 0.03, labels = FALSE, at = x)
-axis(1, tck = 0.06, labels = x_lab, at = x, mgp=c(3, 0.2, 0))
-axis(2, tck = 0.03, mgp=c(3, 0.2, 0), las = 2)
+axis(1, tck = 0.04, labels = x_lab, at = x, mgp=c(3, 0.2, 0))
+axis(2, tck = 0.04, mgp=c(3, 0.2, 0), las = 2)
 
 
-mtext(2, text = do.call(expression, y_lab_gpp), line = c(4.25, 2.25))
+mtext(2, text = do.call(expression, y_lab_gpp), line = 1.5)
 mtext(3, text = "K34 Tower Site in the Amazon Basin", line = 0.5)
+
 legend("topleft", legend=c("K34 ", "ChloFluo", "FluxCom", "FluxSat"),
-       col=c("black", vir.cols[2], vir.cols[4], vir.cols[6]), lty = c(1, 1,1,1),
-       horiz = TRUE, y.intersp = 1, cex = 0.75)
+       col=c("black", vir.cols[2], vir.cols[4], vir.cols[6]), lty = c(1,1,1,1),
+       lwd = c(2,2,2,2), horiz = TRUE, y.intersp = 1, cex = 0.75)
 box()
 
-mtext(1, text = "2019", outer = TRUE, line = 1)
+mtext(1, text = "Month", outer = TRUE, line = 1)
 
 dev.off()
 
